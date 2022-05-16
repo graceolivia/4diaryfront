914c591eb2da1af7e674deff855dd3f31cecae30
Grace <gracelidinskysmith@gmail.com>
2022-05-16 17:34:52 -0400

    Add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is not the greatest app in the world... this is just a tribute
         </p>
         <a
           className="App-link"
