5eb87ba6bffe457925d0edac4d4876beb522a862
Grace <gracelidinskysmith@gmail.com>
2022-05-17 08:30:59 -0400

    Add text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is not the greatest app in the world... this is just a tribute
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Data will appear below in a minute:</p>
       </header>
     </div>
   );
