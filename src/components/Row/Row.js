07baa004a912eed617f87a0032963b975c7a39be
Grace <gracelidinskysmith@gmail.com>
2022-05-19 18:20:54 -0400

    Add new components

---
+++ src/components/Row/Row.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Row.css';
+
+const Row = () => (
+  <div className="Row" data-testid="Row">
+    Row Component
+  </div>
+);
+
+Row.propTypes = {};
+
+Row.defaultProps = {};
+
+export default Row;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Table.css';
+
+const Table = () => (
+  <div className="Table" data-testid="Table">
+    Table Component
+  </div>
+);
+
+Table.propTypes = {};
+
+Table.defaultProps = {};
+
+export default Table;
