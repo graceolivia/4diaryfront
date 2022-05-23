9949f760055444c6f2ebc711ace60445e109476c
Grace <gracelidinskysmith@gmail.com>
2022-05-23 09:33:50 -0400

    Add API call

--- src/components/ApiQuery/ApiQuery.js
+++ src/components/ApiQuery/ApiQuery.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./ApiQuery.css";
 
-const ApiQuery = () => (
-  <div className="ApiQuery" data-testid="ApiQuery">
-    ApiQuery Component
-  </div>
-);
+function ApiQuery() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("http://127.0.0.1:3000/movies/")
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          setMovies(data);
+          console.log(movies);
+        },
+        (error) => {
+          setMovies(error);
+          console.log(movies);
+        }
+      );
+  }, []);
+  return (
+    <div className="ApiQuery" data-testid="ApiQuery">
+      [ {movies[0]} ]
+    </div>
+  );
+}
 
 ApiQuery.propTypes = {};
 
