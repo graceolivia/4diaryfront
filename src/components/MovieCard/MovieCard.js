07144fb6f2b3981e8309372a0f4f5cf10b3b9beb
Grace <gracelidinskysmith@gmail.com>
2022-05-24 12:16:50 -0400

    Card

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,9 +5,15 @@ import "./MovieCard.css";
 function MovieCard(props) {
   const { title, genre } = props;
   return (
-    <div>
-      <p>Title: {title}</p>
+    <div id="MovieCard">
+      <h3>Title: {title}</h3>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/en/b/b6/Tenacious_d_in_the_pick_of_destiny_ver3.jpg"
+        id="MovieImage"
+      />
       <p>Genre: {genre}</p>
+      <p>Release Date: </p>
+      <p>Who does Jables Play: </p>
     </div>
   );
 }
