cb939b33302382659fe3b0995e0bca041a579cbb
Grace <gracelidinskysmith@gmail.com>
2022-05-24 15:08:30 -0400

    Add alt text

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./MovieCard.css";
 
 function MovieCard(props) {
@@ -10,6 +9,7 @@ function MovieCard(props) {
       <img
         src="https://upload.wikimedia.org/wikipedia/en/b/b6/Tenacious_d_in_the_pick_of_destiny_ver3.jpg"
         id="MovieImage"
+        alt="Tenacious D and the Pick of Destiny"
       />
       <p>Genre: {genre}</p>
       <p>Release Date: </p>
