7ba065adea47d556469d020bf5d9b2a63ffd0050
Grace <gracelidinskysmith@gmail.com>
2022-06-09 13:21:36 -0400

    Add title

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>The Jables Review Source</h1>
+        <h4>Your source for only Jack Black movie reviews</h4>
         <img src={tenacious} alt="Tenacious D" />
         <p>
           This is not the greatest app in the world...{" "}
           <span id="cursive">this is just a tribute</span>
         </p>
-        <ApiQuery />
+        {/* <ApiQuery /> */}
       </header>
     </div>
   );
